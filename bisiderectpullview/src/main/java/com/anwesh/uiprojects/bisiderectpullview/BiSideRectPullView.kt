271d13da005ea45e3641f4bae5baa151af8a991b
Anwesh43 <anweshthecool0@gmail.com>
2019-12-31 01:38:02 +0530

    adding fixes

--- bisiderectpullview/src/main/java/com/anwesh/uiprojects/bisiderectpullview/BiSideRectPullView.kt
+++ bisiderectpullview/src/main/java/com/anwesh/uiprojects/bisiderectpullview/BiSideRectPullView.kt
@@ -26,11 +26,11 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n: Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
-fun Float.cosify() : Float = Math.sin(Math.PI / 2 + (Math.PI / 2) * this).toFloat()
+fun Float.cosify() : Float = 1f - Math.sin(Math.PI / 2 + (Math.PI / 2) * this).toFloat()
 
 fun Canvas.drawSideRectPull(i : Int, scale : Float, size : Float, w : Float, paint : Paint) {
     val sf : Float = scale.sinify().divideScale(i, sides)
-    val sc : Float = scale.divideScale(1, 2).cosify().divideScale(i, sides)
+    val sc : Float = scale.divideScale(1, 2).cosify().divideScale(sides - 1 - i, sides)
     save()
     scale(1f - 2 * i, 1f)
     paint.color = foreColor1
