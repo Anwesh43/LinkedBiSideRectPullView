6c25b34b232e5ff424d8ff43ab46aa63ab31e6c7
Anwesh43 <anweshthecool0@gmail.com>
2019-12-31 00:45:42 +0530

    adding constants and libraries

--- bisiderectpullview/src/main/java/com/anwesh/uiprojects/bisiderectpullview/BiSideRectPullView.kt
+++ bisiderectpullview/src/main/java/com/anwesh/uiprojects/bisiderectpullview/BiSideRectPullView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.bisiderectpullview
 /**
  * Created by anweshmishra on 31/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val sides : Int = 2
+val scGap : Float = 0.02f / sides
+val strokeFactor : Int = 90
+val hFactor : Float = 5f
+val foreColor1 : Int = Color.parseColor("#3F51B5")
+val foreColor2 : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
