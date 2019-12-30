eb2a9a5c7c089f3196a022e6045a639adb68e237
Anwesh43 <anweshthecool0@gmail.com>
2019-12-31 01:09:32 +0530

    adding BiSideRectPull class

--- bisiderectpullview/src/main/java/com/anwesh/uiprojects/bisiderectpullview/BiSideRectPullView.kt
+++ bisiderectpullview/src/main/java/com/anwesh/uiprojects/bisiderectpullview/BiSideRectPullView.kt
@@ -163,4 +163,27 @@ class BiSideRectPullView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiSideRectPull(var i : Int) {
+
+        private var curr : BSRPNode = BSRPNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
