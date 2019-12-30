28ecf07dc5953937e2637fb6989a54d49ff0bc66
Anwesh43 <anweshthecool0@gmail.com>
2019-12-31 01:01:53 +0530

    adding BiSideRectPullView class

--- bisiderectpullview/src/main/java/com/anwesh/uiprojects/bisiderectpullview/BiSideRectPullView.kt
+++ bisiderectpullview/src/main/java/com/anwesh/uiprojects/bisiderectpullview/BiSideRectPullView.kt
@@ -56,3 +56,21 @@ fun Canvas.drawBSRPNode(i : Int, scale : Float, paint : Paint) {
     drawBiSideRectPull(scale, size, w, paint)
     restore()
 }
+
+class BiSideRectPullView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
